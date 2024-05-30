bdb7398d6801426ee8e36c7622d662d387d585ae
Denis Ivanov <denisivanov@Deniss-MacBook-Pro-2.local>
2024-05-30 11:05:56 -0500

    Player List

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 'use client';
-import { Flex,Box,Text } from "@chakra-ui/react";
+import { Flex,Box,Text, VStack } from "@chakra-ui/react";
 import Navbar from "./Components/NavBar";
+import PlayersList from "./Components/PlayerList";
 
 const HomePage: React.FC = () => {
 
     return (
         <>
         <Navbar />
-            <Text fontSize="5xl">Hello World</Text>
+        <Box width={'100%'} height={'100%'}>
+        <PlayersList players={[{address: '0x123', amount: 100}, {address: '0x456', amount: 200}, {address: '0x789', amount: 300}, {address: '0xabc', amount: 400}]} />
+        </Box>
         </>
     );
 };
