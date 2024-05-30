fe6262e7ae91b5b6e6f80fa26aaf7067fec865c0
jicodes <liu.ji1@northeastern.edu>
2024-05-30 11:29:01 -0500

    remove unused navbar componenton home page

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ const HomePage: React.FC = () => {
         <Box width={'100%'} height={'100%'}>
         <PlayersList players={[{address: '0x123', amount: 100}, {address: '0x456', amount: 200}, {address: '0x789', amount: 300}, {address: '0xabc', amount: 400}]} />
         </Box>
-            <Navbar />
         </>
     );
 };
